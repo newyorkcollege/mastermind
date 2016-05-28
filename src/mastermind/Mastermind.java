9d4a7a78efcd434b50652467cf2aed8c333d6a68
George Prokopakis <gprok@Georges-MacBook-Pro-2.local>
2016-05-28 13:50:31 +0300

    Initial commit of the project

---
+++ src/mastermind/Mastermind.java
@@ -0,0 +1,12 @@
+package mastermind;
+
+/**
+ * Logic of the mastermind game
+ */
+public class Mastermind {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
